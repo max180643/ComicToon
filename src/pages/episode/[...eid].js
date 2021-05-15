4a48bb32950c9978e329ddf01ffb58ddbb724bcb
max180643 <max180643@gmail.com>
2021-05-16 01:53:48 +0700

    :rocket: add pdf viewer

--- src/pages/episode/[...eid].js
+++ src/pages/episode/[...eid].js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { Document, Page, pdfjs } from 'react-pdf'
+
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js'
 
 const apiData = {
   apiPath: process.env.NEXT_PUBLIC_API_PATH
@@ -9,6 +12,11 @@ const episodeAdd = () => {
   const router = useRouter()
 
   const [episodeData, setEpisodeData] = useState([])
+  const [numPages, setNumPages] = useState(null)
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages)
+  }
 
   useEffect(() => {
     if (router.isReady) {
@@ -31,7 +39,20 @@ const episodeAdd = () => {
     }
   }, [router.isReady])
 
-  return <div>{episodeData.path}</div>
+  return (
+    <div className="all-page-container">
+      <Document file={episodeData.path} onLoadSuccess={onDocumentLoadSuccess}>
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+            renderTextLayer={false}
+            className="d-flex justify-content-center"
+          />
+        ))}
+      </Document>
+    </div>
+  )
 }
 
 export default episodeAdd
